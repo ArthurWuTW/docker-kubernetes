a49c31e2cb4fb008dd5b77507165ae8e8c007bb3
Arthur <lion811004@gmail.com>
2022-12-14 13:16:29 +0800

    install kubernetes

--- install_kubernetes.sh
+++ install_kubernetes.sh
@@ -14,3 +14,21 @@ sudo add-apt-repository \
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" >> ~/kubernetes.list
+
+sudo mv ~/kubernetes.list /etc/apt/sources.list.d
+
+sudo apt update
+
+sudo apt install -y kubelet
+
+sudo apt install -y kubeadm
+
+sudo apt install -y kubectl
+
+sudo apt-get install -y kubernetes-cni
+
+
+
