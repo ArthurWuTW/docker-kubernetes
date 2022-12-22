3fd0ff1dacc67447f6abcaa4198ee33d085b3ff1
arthurtwks <lion811004@gmail.com>
2022-12-22 10:24:35 +0800

    remanage script

--- centos-start-k8s.sh
+++ centos-start-k8s.sh
@@ -12,20 +12,21 @@ yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 yum install -y docker-ce docker-ce-cli containerd.io
 
-
-swapoff -a
-systemctl daemon-reload
-systemctl restart docker
-systemctl restart kubelet
-# need to wait for about 10 mins, you will see kubelet start, before that you always see kubelet not starting by command 'service kubelet status'
+# stop firewalld
+service firewalld stop
 
 # install kubernetes
 touch /etc/yum.repos.d/kubernetes.repo
 vi /etc/yum.repos.d/kubernetes.repo # paste from file
 yum install -y kubelet kubeadm kubectl
 
-# stop firewalld
-service firewalld stop
+# kubelet will be dead if not doing this
+swapoff -a
+sed -i '/ swap / s/^/#/' /etc/fstab
+systemctl daemon-reload
+systemctl restart docker
+systemctl restart kubelet
+# need to wait for about 10 mins, you will see kubelet start, before that you always see kubelet not starting by command 'service kubelet status'
 
 # remove containerd toml file
 rm -f /etc/containerd/config.toml
@@ -33,7 +34,6 @@ service containerd restart
 
 # pull kubeadm images
 kubeadm config images pull
-
 
 # init kubeadm
 kubeadm init --apiserver-advertise-address=172.20.10.6 --image-repository registry.aliyuncs.com/google_containers --kubernetes-version v1.26.0 --service-cidr=10.96.0.0/12 --pod-network-cidr=10.244.0.0/16
